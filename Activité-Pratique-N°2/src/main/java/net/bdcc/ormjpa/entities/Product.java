ea7126fa2c816a62aad1406ad3747f2e0c4a9fa4
Youssef Belahrach <y.belahrach@gmail.com>
2026-07-05 01:08:58 +0100

    Commit de l'Activité Pratique 2

---
+++ Activité-Pratique-N°2/src/main/java/net/bdcc/ormjpa/entities/Product.java
@@ -0,0 +1,18 @@
+package net.bdcc.ormjpa.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Setter @Getter @ToString @NoArgsConstructor @AllArgsConstructor @Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private long quantity;
+}
---
+++ Activité-Pratique-N°2/src/test/java/net/bdcc/ormjpa/OrmJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bdcc.ormjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
